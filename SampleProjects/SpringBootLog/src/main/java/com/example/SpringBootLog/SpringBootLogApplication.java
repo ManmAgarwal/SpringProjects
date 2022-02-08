f578331680f40fb5d581af4f676a86302893f001
ManmAgarwal <agarwal.manmath@gmail.com>
2022-02-08 19:17:19 +0530

    Medium Workspaces
    Medium Workspaces

---
+++ SampleProjects/SpringBootLog/src/main/java/com/example/SpringBootLog/SpringBootLogApplication.java
@@ -0,0 +1,20 @@
+package com.example.SpringBootLog;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLogApplication {
+
+	static Logger log = Logger.getLogger("SpringBootLogApplication.class");
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLogApplication.class, args);
+		
+		log.log(Level.INFO,"Logging Info Message");
+		log.log(Level.SEVERE,"Logging Severe Message");
+		log.log(Level.WARNING,"Logging Warning Message");
+	}
+}
---
+++ SampleProjects/SpringBootLog/src/test/java/com/example/SpringBootLog/SpringBootLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootLog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
